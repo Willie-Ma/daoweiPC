4bec899f009234b72e4bbd27ccf812aff30f0665
mmm <mxj1323610522@163.com>
2017-10-30 08:38:17 +0800

    daowei

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+
+import $ from 'jquery';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap';
+import '../css/index.css'
+
+import 'Swiper/dist/css/swiper.css';
+import Swiper from 'Swiper/dist/js/swiper';
+
+
